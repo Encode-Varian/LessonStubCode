7a81d0160c3b06af3a8ed35ebefb15eee08a0347
Encode-Varian <80142319+Encode-Varian@users.noreply.github.com>
2025-05-20 12:12:06 +1000

    Stub of Ch.12

---
+++ EX-COND/Ch12.py
@@ -0,0 +1,40 @@
+import random
+
+#Look of the Paper
+def paper():
+  print("xxxxxxx")
+  print("x     x")
+  print("x     x")
+  print("x     x")
+  print("xxxxxxx")
+
+#Look of the Scissors
+def scissors():
+  print("xx   xx")
+  print("xx   xx")
+  print("   xx  ")
+  print("   xx  ")
+  print("xxxxxxx")
+
+#Look of the Rock
+def rock():
+  print("  xxxx ")
+  print("xxxxxxx")
+  print("xxxxxxx")
+  print("xxxxxxx")
+  print("  xxx  ")
+
+# Get player input
+## Code Here ##
+
+# Display the player input using condition
+## Code Here ##
+
+# Random the computer's choice
+computer = random.randrange(1,4)
+
+# Display the computer's choice using condition
+## Code Here ##
+
+# Display the result using condition
+## Code Here ##
